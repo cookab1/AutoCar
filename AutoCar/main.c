aee787f68dda96508cfbab0abaa454386fd9e110
Cook <cookab1@appd.appstate.edu>
2018-04-24 14:57:34 -0400

    init commit

---
+++ AutoCar/main.c
@@ -0,0 +1,18 @@
+/*
+ * AutoCar.c
+ *
+ * Created: 4/24/2018 2:56:04 PM
+ * Author : cookab1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
